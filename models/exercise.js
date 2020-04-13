264ac2d83997b7d014bcadefdecb06bd9f82b6ba
markgalante <mark.galante.14@gmail.com>
2020-04-13 21:04:09 +0200

    add slug to exercise model

--- models/exercise.js
+++ models/exercise.js
@@ -31,4 +31,46 @@ const exerciseSchema = new mongoose.Schema({
     ]
 });
 
+var Exercise = mongoose.model("Exercise", exerciseSchema); 
+
+exerciseSchema.pre('save', async function(next){
+    try{
+        if(this.isNew || this.wasNew){
+            this.slug = await generateUniqueSlug(this._id, this.name); 
+        }
+        next();
+    } catch(err){
+        console.log(err.message); 
+    }
+}); 
+
+async function generateUniqueSlug(id, exerciseName, slug){
+    try{
+        if(!slug){
+            return slug = slugify(exerciseName); //added return   
+        }
+        var exercise = await Exercise.findOne({slug:slug}); 
+        if(!exercise || exercise._id.equals(id)){
+            return slug; 
+        }
+        var newSlug = slugify(exerciseName); 
+        return await generateUniqueSlug(id, exerciseName, newSlug); 
+    } catch(err){
+        console.log(err.message); 
+    }
+}
+
+function slugify(text){
+    var slug = text
+    .toString()
+    .toLowerCase()
+    .replace(/\s+/g, '-')        // Replace spaces with -
+    .replace(/[^\w\-]+/g, '')    // Remove all non-word chars
+    .replace(/\-\-+/g, '-')      // Replace multiple - with single -
+    .replace(/^-+/, '')          // Trim - from start of text
+    .replace(/-+$/, '')          // Trim - from end of text
+    .substring(0, 75);           // Trim at 75 characters
+    return slug + "-" + Math.floor(1000 + Math.random() * 9000) //Add 4 random digits to improve uniqueness
+}
+
 module.exports = mongoose.model('Exercise', exerciseSchema); 
