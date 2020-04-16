b150d01203e6defef3fbe560d89e6afbf48daecf
markgalante <mark.galante.14@gmail.com>
2020-04-16 17:45:29 +0200

    move routes to bottom of page to enable passport

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const   express         = require('express'),
         mongoose        = require('mongoose'),
         bodyParser      = require('body-parser'),
         passport        = require('passport'), 
-        LocalStrategy   = require('passport-local'), 
-        session         = require('express-session'),   
+        passportLocalMongoose = require('passport-local-mongoose'), 
+        LocalStrategy   = require('passport-local'),    
         User            = require('./models/user');
 
 //REQUIRE ROUTE FILES:
@@ -12,29 +12,37 @@ const   exerciseRoutes  = require('./routes/exercises'),
         commentRoutes   = require('./routes/comments'),
         indexRoutes     = require('./routes/index'); 
 
-app.use('/', indexRoutes); 
-app.use('/exercises', exerciseRoutes); 
-app.use('/exercises/:slug/comments', commentRoutes);
-
 //CONFIGURATION OF mongoose, bodyParser, ejs and setting the use of public folders for CSS; 
 mongoose.connect("mongodb://localhost/kinetic", {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }); 
-app.use(bodyParser.urlencoded( {extended:true} )); 
+app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs'); 
 app.use(express.static(__dirname + '/public')); 
 
-//CONFIGURE PASSPORT: 
-app.use(session({
-    secret: "Any message can go here apparently", resave:false, saveUninitialized: false
-}));
+//PASSPORT CONFIG 
+app.use(require("express-session")({ 
+    secret: "Any message can go here apparently",   
+    resave: false, 
+    saveUninitialized: false
+})); 
 app.use(passport.initialize()); 
 app.use(passport.session()); 
-passport.use(new LocalStrategy(User.authenticate())); 
+passport.use(new LocalStrategy(User.authenticate())); //comes from passport-local-mongoose
+passport.use(User.createStrategy()); 
 passport.serializeUser(User.serializeUser()); 
-passport.deserializeUser(User.deserializeUser()); 
+passport.deserializeUser(User.deserializeUser());
 
 app.get('/', (req, res)=>{
     res.send("HOME PAGE!")
 }); 
+
+app.use((req, res, next)=>{
+    res.locals.currentUser = req.user; 
+    next()
+});
+
+app.use('/', indexRoutes); 
+app.use('/exercises', exerciseRoutes); 
+app.use('/exercises/:slug/comments', commentRoutes);
 
 //SET UP PORT: 
 app.listen(3000, ()=>{
