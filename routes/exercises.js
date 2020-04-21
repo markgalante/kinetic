0de84aabed1ae171ecb37b237ece7cc58bba644f
markgalante <mark.galante.14@gmail.com>
2020-04-21 08:05:00 +0200

    Added multer

--- routes/exercises.js
+++ routes/exercises.js
@@ -1,18 +1,39 @@
 const   express     = require('express'), 
         router      = express.Router(), 
         multer      = require('multer'),
+        cloudinary  = require('cloudinary'), 
         Exercise    = require('../models/exercise'),
-        Comment     = require('../models/comment');
+        Comment     = require('../models/comment'),
+        muscles     = require ('../public/muscles.json'); 
 
-const muscles = require ('../public/muscles.json'); 
+//CONFIGURE MULTER: 
+const storage = multer.diskStorage({
+    filename: (req, file, callback)=>{
+        callback(null, Date.now() + file.originalname); 
+    }
+});
+const imageFilter = (req, file, callback) => {
+    if(!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)){ //If the file name is NOT the following.
+        return callback(new Error('Only image files are allowed.'), false); 
+    }  
+    callback(null, true); 
+};
+const upload = multer({storage: storage, fileFilter: imageFilter});
+
+//setting up cloudinary
+cloudinary.config({ 
+    cloud_name: 'dbpkz1rnm', 
+    api_key: 479674634572721 /*process.env.CLOUDINARY_API_KEY*/, 
+    api_secret: 'zyBsxCMOAqCEZ-BCVr3Rc2GjBZw' /*process.env.CLOUDINARY_API_SECRET*/ 
+});
 
 // EXERCISE INDEX ROUTE 
 router.get('/', (req, res)=>{
-    Exercise.find({}, (err, exercise)=>{
+    Exercise.find({}, (err, exercises)=>{
         if(err){
             console.log(err); 
         }
-        res.render('./exercises/index', {exercise:exercise}); 
+        res.render('./exercises/index', {exercises:exercises, muscles:muscles}); 
     }); 
 }); 
 
@@ -22,15 +43,26 @@ router.get('/new', (req, res)=>{
 }); 
 
 // NEW EXERCISE POST ROUTE 
-router.post('/', (req, res)=>{
+router.post('/', upload.single('image'), (req, res)=>{
+    const ref= {
+        authors: req.body.authors,  
+        year: req.body.year, 
+        title: req.body.title, 
+        journal: req.body.journal, 
+        edition: req.body.edition, 
+        pageStart: req.body.pageStart,
+        pageEnd: req.body.pageEnd
+    }
+
     const exercise = new Exercise({
-        name: req.body.name, 
+        name: req.body.name,
+        description: req.body.description,
+        muscle: req.body.muscle,
+        reference: {$push: {$each: ref}}
     }); 
-    console.log("REQ BODY NAME BEFORE CREATE" + req.body.name)
     Exercise.create(exercise, (err, newlyCreate)=>{
         if(err){
-            console.log("EXERCISE CREATE ERROR MESSAGE: " + err);
-            console.log("REQ.BODY.EXERCISE: " + req.body.exercise); 
+           console.log("ERROR ADDING EXERCISE: " + err); 
             return res.redirect("back");  
         }
         console.log(newlyCreate); 
