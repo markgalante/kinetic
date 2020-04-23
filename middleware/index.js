6c81fd052caf938d3ce091fc612ab388ee405d1d
markgalante <mark.galante.14@gmail.com>
2020-04-23 16:19:38 +0200

    Create middleware route

---
+++ middleware/index.js
@@ -0,0 +1,13 @@
+const   middlewareObj   = {}, 
+        Exercise        = require('../models/exercise'), 
+        Comment         = require('../models/comment'), 
+        User            = require('../models/user'); 
+
+middlewareObj.isLoggedIn = (req, res, next) => {
+    if(req.isAuthenticated()){
+        return next(); 
+    } else {
+        res.redirect('/login'); 
+        console.log('You need to be logged in to do that.');
+    }
+}
