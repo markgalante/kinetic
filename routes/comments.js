c93d3a9bdb0986a6456b1f72927ab2f87f82752b
markgalante <mark.galante.14@gmail.com>
2020-04-23 22:00:49 +0200

    Create route for comment creation

--- routes/comments.js
+++ routes/comments.js
@@ -1,10 +1,37 @@
 const   express     = require('express'), 
-        router      = express.Router(), 
+        router      = express.Router({mergeParams: true}), 
         Exercise    = require('../models/exercise'),
-        Comment     = require('../models/comment');
+        Comment     = require('../models/comment'), 
+        User        = require('../models/user'), 
+        middleware  = require('../middleware/index'); 
 
-router.get('/', (req, res)=>{
-    res.send('COMMENT ROUTE WORKING!'); 
+router.post('/', middleware.isLoggedIn, (req, res)=>{
+    Exercise.findOne({slug: req.params.slug}, (err, foundExercise)=>{
+        if(err){
+            console.log('CANNOT FIND EXERCISE TO COMMENT ON: ' + err.message);
+            res.redirect('back');  
+        }
+        Comment.create(req.body.comment, (err, comment)=>{
+            if(err){
+                console.log('UNABLE TO CREATE COMMENT: ' + err.message); 
+                return res.redirect('back')
+            }
+            //add username and id to comments. 
+            comment.author.id = req.user._id; 
+            comment.author.username = req.user.username;
+
+            //save comment: 
+            comment.save(); 
+
+            //push comments to found exercise. 
+            foundExercise.comments.push(comment); 
+            foundExercise.save(); 
+
+            //redirect back; 
+            console.log('SAVED COMMENT: ' + comment); 
+            res.redirect('back'); 
+        }); 
+    }); 
 }); 
 
 module.exports = router; 
